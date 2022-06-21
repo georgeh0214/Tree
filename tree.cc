d4a0b73b1dafc1e33271c7551ecae39b81a5cc6d
georgeh0214 <george980214@gmail.com>
2022-06-21 15:52:08 -0700

    more asserts

--- tree.cc
+++ tree.cc
@@ -320,7 +320,8 @@ RetryInsert:
             assert(idx >= 1 && idx <= leaf->count());
             new_leaf->appendKeyEntry(leaf->getKey(idx), leaf->getLen(idx), leaf->ent[idx]);
         }
-        printf("Leaf count: %d    i: %d \n", cnt, i);
+        printf("Leaf count: %d   meta size: %d     i: %d \n", cnt, leaf->meta_size, i);
+        printf("New leaf should have: %d keys \n", cnt - i + 1);
         assert(new_leaf->count() == (cnt - i + 1));
         // set split key
         idx = sorted_pos[i++]; // index of split key
